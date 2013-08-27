a542f4f8ce531e1359e1ff85f738e41c71b4a323
Marembo Ochieng <marembo2008@gmail.com>
2013-08-27 16:11:17 +0300

    validator

---
+++ src/main/java/com/anosym/jflemax/jsf/validator/PositiveNumberValidator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.anosym.jflemax.jsf.validator;
+
+import java.math.BigDecimal;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author marembo
+ */
+@FacesValidator("positiveNumberValidator")
+public class PositiveNumberValidator implements Validator {
+
+  public void validate(FacesContext fc, UIComponent uic, Object value) throws ValidatorException {
+    try {
+      if (new BigDecimal(value.toString()).signum() < 1) {
+        FacesMessage msg = new FacesMessage("Validation failed.",
+                "Number must be strictly positive");
+        msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+        throw new ValidatorException(msg);
+      }
+    } catch (NumberFormatException ex) {
+      FacesMessage msg = new FacesMessage("Validation failed.", "Not a number");
+      msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+      throw new ValidatorException(msg);
+    }
+  }
+}
