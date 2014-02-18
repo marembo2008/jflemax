99a65f3c3bdd9118f36d691cae095dee9b9ea20c
Marembo Ochieng <marembo2008@gmail.com>
2014-02-18 10:37:05 +0300

    commaSeparatedListOfStringConverter

--- src/main/java/com/anosym/jflemax/jsf/converter/CommaSeparatedListOfStringConverter.java
+++ src/main/java/com/anosym/jflemax/jsf/converter/CommaSeparatedListOfStringConverter.java
@@ -4,6 +4,7 @@
  */
 package com.anosym.jflemax.jsf.converter;
 
+import com.anosym.jflemax.JFlemaxLogger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,26 +18,21 @@ import javax.faces.convert.FacesConverter;
  * @author marembo
  */
 @FacesConverter("commaSeparatedListOfStringConverter")
-public class CommSeparatedListOfStringConverter implements Converter {
+public class CommaSeparatedListOfStringConverter implements Converter {
 
   public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+    JFlemaxLogger.fine("Getting list for: " + string);
     if (string != null) {
-      return new ArrayList<String>(Arrays.asList(string.split(",")));
+      return new ArrayList<String>(Arrays.asList(string.split(",\\s*")));
     }
     return null;
   }
 
   public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+    JFlemaxLogger.fine("Getting string from list: " + o);
     if (o != null && List.class.isAssignableFrom(o.getClass())) {
-      List<String> str = (List<String>) o;
-      String ss = "";
-      for (String s : str) {
-        if (!ss.isEmpty()) {
-          ss += ",";
-        }
-        ss += s;
-      }
-      return ss;
+      String str = o.toString();
+      return str.substring(1, str.length() - 1);
     }
     return null;
   }
